81c8b2531c6736ed679c743235167816666d035a
Georjane <pzgeorjane@gmail.com>
2020-08-19 11:22:52 +0100

    Changed name attributes in form to submit a user hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
@@ -11,9 +12,9 @@ class UsersController < ApplicationController
     end
   end
 
-  # private
+  private
   
-  # def user_params
-  #   params.require(:user).permit(:username, :email, :password)
-  # end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
