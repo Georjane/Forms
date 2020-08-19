9ad4ba0a9cc6a860dbde982a46dea33df7c30347
Georjane <pzgeorjane@gmail.com>
2020-08-19 10:01:29 +0100

    Used helper method user_params with require and permit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
