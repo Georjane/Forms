f43cfd756044b35a94b394bab10ff538da021dad
Georjane <pzgeorjane@gmail.com>
2020-08-19 14:04:26 +0100

    Remove trailing whitespace

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])  
+    @user = User.find(params[:id])
   end
 
   def update
@@ -26,11 +26,11 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :edit
-    end  
+    end
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:username, :email, :password, :id)
   end
