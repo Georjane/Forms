9d7e65846b67515ee4d995bd8f7db0c8505ad0a7
Georjane <pzgeorjane@gmail.com>
2020-08-19 11:51:39 +0100

    Used form_for tag to create form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def create
     @user = User.new(username: params[:username], email: params[:email], password: params[:password])
