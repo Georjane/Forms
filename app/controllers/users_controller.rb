dc94cb3b3d9ac2dc0af15c373dbffdf3f7763187
Georjane <pzgeorjane@gmail.com>
2020-08-19 11:36:24 +0100

    Used form_tag to create form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    @user = User.new(user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
