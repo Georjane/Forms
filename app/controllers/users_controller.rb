71ea740e55854bacf3205c9ae9cf5dc25e3085e6
Georjane <pzgeorjane@gmail.com>
2020-08-19 10:58:09 +0100

    Can create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
     if @user.save
       redirect_to new_user_path
@@ -11,7 +11,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
-  end
+  # private
+  
+  # def user_params
+  #   params.require(:user).permit(:username, :email, :password)
+  # end
 end
